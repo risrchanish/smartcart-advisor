20a1319da5aa073d668560ad2b0d36d85d928d16
risrchanish <risrchanish@gmail.com>
2025-08-29 07:27:19 +0530

    add product entity with metadata and ratings

---
+++ entity/Product.java
@@ -0,0 +1,163 @@
+package risrchanish.product.recommend.entity;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long productId;
+	
+	private String name;
+	private String category;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "metadata_id" , referencedColumnName = "metadataId")
+	private ProductMetadata metadata;
+	
+	
+	private double price;
+	private boolean inStock;
+	
+	@OneToMany(mappedBy = "product",cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Rating> ratings;
+	
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<ProductFeature> features; 
+
+
+	public Product()
+	{}
+	
+	public Long getProductId() {
+		return productId;
+	}
+	
+	
+	// Product Id excluded
+	
+	public Product(String name, String category, ProductMetadata metadata, double price,
+			boolean inStock, List<Rating> ratings, List<ProductFeature> features) {
+
+		this.name = name;
+		this.category = category;
+		this.metadata = metadata;
+		this.price = price;
+		this.inStock = inStock;
+		this.ratings = ratings;
+		this.features = features;
+	}
+
+
+	// All arg constructor
+	public Product(Long productId, String name, String category, ProductMetadata metadata, double price,
+			boolean inStock, List<Rating> ratings, List<ProductFeature> features) {
+
+		this.productId = productId;
+		this.name = name;
+		this.category = category;
+		this.metadata = metadata;
+		this.price = price;
+		this.inStock = inStock;
+		this.ratings = ratings;
+		this.features = features;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public ProductMetadata getMetadata() {
+		return metadata;
+	}
+
+	public void setMetadata(ProductMetadata metadata) {
+		this.metadata = metadata;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public boolean isInStock() {
+		return inStock;
+	}
+
+	public void setInStock(boolean inStock) {
+		this.inStock = inStock;
+	}
+
+	public List<Rating> getRatings() {
+		return ratings;
+	}
+
+	public void setRatings(List<Rating> ratings) {
+		this.ratings = ratings;
+	}
+
+	public List<ProductFeature> getFeatures() {
+		return features;
+	}
+
+	public void setFeatures(List<ProductFeature> features) {
+		this.features = features;
+	}
+	
+//	public void setProductId(Long productId) {
+//	this.productId = productId;
+//}
+
+
+	@Override
+	public String toString() {
+		
+		List<Long> ratingsIds = ratings != null ?
+								ratings.stream().map(rating -> rating.getRatingId())
+								.collect(Collectors.toList())
+								: Collections.emptyList();
+		
+		List<Long> featuresIds = features != null ?
+								features.stream().map(feature -> feature.getFeatureId())
+								.collect(Collectors.toList()) :
+									Collections.emptyList();
+
+		
+		return "Product [productId=" + productId + ", name=" + name + ", category=" + category + 
+				", metadata=" + (metadata != null ? metadata.getMetadataId() : null) + 
+				", price=" + price + ", inStock=" + inStock + 
+				", ratingsIds="  + ratingsIds + 
+				", featuresIds=" + featuresIds + "]";
+	}
+	
+	
+}

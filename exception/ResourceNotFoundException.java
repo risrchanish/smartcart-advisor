5a779d8ea6817512dca1cf154637c29b65428bed
risrchanish <risrchanish@gmail.com>
2025-08-31 15:59:26 +0530

    Add custom ResourceNotFoundException for entity lookup failures

---
+++ exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package risrchanish.product.recommend.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+	
+	public ResourceNotFoundException(String message)
+	{
+		super(message);
+	}
+
+}

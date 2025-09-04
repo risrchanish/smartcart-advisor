3d8947a857470fbd7d097249fd0f2fcefcad6abb
risrchanish <risrchanish@gmail.com>
2025-09-04 18:51:53 +0530

    Add null check

--- productfeatureDto/ProductFeatureCreateDto.java
+++ productfeatureDto/ProductFeatureCreateDto.java
@@ -2,7 +2,7 @@ package risrchanish.product.recommend.dto.productfeature;
 
 import java.util.List;
 
-import risrchanish.product.recommend.entity.Product;
+
 
 public class ProductFeatureCreateDto {
 

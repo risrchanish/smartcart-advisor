71cc3388b556e7df52001e15eb4bfd4db2f4cc53
risrchanish <risrchanish@gmail.com>
2025-09-04 18:54:46 +0530

    Add null checks

--- productDto/ProductCreateDto.java
+++ productDto/ProductCreateDto.java
@@ -1,5 +1,13 @@
 package risrchanish.product.recommend.dto.product;
 
+
+import java.util.List;
+
+import risrchanish.product.recommend.dto.productfeature.ProductFeatureCreateDto;
+import risrchanish.product.recommend.dto.productmetadata.ProductMetadataCreateDto;
+import risrchanish.product.recommend.dto.rating.RatingCreateDto;
+
+
 public class ProductCreateDto {
 
 //	@NotBlank(message = "Product name is required")
@@ -14,8 +22,40 @@ public class ProductCreateDto {
 	
 	private boolean inStock;
 	
+	private ProductMetadataCreateDto metadata;
+	
+	private List<RatingCreateDto> ratings;
+	
+	private List<ProductFeatureCreateDto> featureVector;
+	
+	
+	public List<RatingCreateDto> getRatings() {
+		return ratings;
+	}
+
+	public void setRatings(List<RatingCreateDto> ratings) {
+		this.ratings = ratings;
+	}
+
+	public List<ProductFeatureCreateDto> getFeatures() {
+		return featureVector;
+	}
+
+	public void setFeatures(List<ProductFeatureCreateDto> featureVector) {
+		this.featureVector = featureVector;
+	}
+
+	public ProductMetadataCreateDto getMetadata() {
+		return metadata;
+	}
+
+	public void setMetadata(ProductMetadataCreateDto metadata) {
+		this.metadata = metadata;
+	}
+
+
 	public ProductCreateDto(String name, Double price, String category, boolean inStock) {
-		super();
+		
 		this.name = name;
 		this.price = price;
 		this.category = category;
@@ -58,8 +98,9 @@ public class ProductCreateDto {
 	@Override
 	public String toString() {
 		return "ProductCreateDto [name=" + name + ", price=" + price + ", category=" + category + ", inStock=" + inStock
-				+ "]";
+				+ ", ratings=" + ratings + ", features=" + featureVector + ", metadata=" + metadata + "]";
 	}
+
 
 	
 }

7d32bfcf91483dea5d3eb75a1e2bb8a60da0511f
risrchanish <risrchanish@gmail.com>
2025-09-08 15:36:13 +0530

    Service with full CRUD, pagination, and validation

--- service/Impl/PreferenceServiceImpl.java
+++ service/Impl/PreferenceServiceImpl.java
@@ -1,52 +1,125 @@
 package risrchanish.product.recommend.service.Impl;
 
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import risrchanish.product.recommend.dto.preference.PreferenceDto;
+import risrchanish.product.recommend.dto.preference.PreferenceCreateDto;
+import risrchanish.product.recommend.dto.preference.PreferenceResponseDto;
+import risrchanish.product.recommend.dto.preference.PreferenceUpdateDto;
+import risrchanish.product.recommend.entity.Preference;
+import risrchanish.product.recommend.entity.User;
+import risrchanish.product.recommend.exception.ResourceNotFoundException;
+import risrchanish.product.recommend.mapper.PreferenceMapper;
 import risrchanish.product.recommend.repository.PreferenceRepository;
+import risrchanish.product.recommend.repository.UserRepository;
 import risrchanish.product.recommend.service.PreferenceService;
 
 @Service
 public class PreferenceServiceImpl implements PreferenceService{
 	
 	private PreferenceRepository preferenceRepository;
-
-	@Override
-	public PreferenceDto createPreference(PreferenceDto dto) {
-		// TODO Auto-generated method stub
-		return null;
+	private UserRepository userRepository;
+	
+	public PreferenceServiceImpl(PreferenceRepository preferenceRepository, UserRepository userRepository)
+	{
+		this.preferenceRepository = preferenceRepository;
+		this.userRepository = userRepository;
 	}
 
 	@Override
-	public PreferenceDto updatePreference(Long preferenceId, PreferenceDto dto) {
-		// TODO Auto-generated method stub
-		return null;
+	public PreferenceResponseDto createPreference(PreferenceCreateDto dto) {
+		
+		User user = userRepository.findById(dto.getUserId())
+						.orElseThrow(() -> new ResourceNotFoundException("User not found"));
+		
+		Preference preference = PreferenceMapper.toEntity(dto, user);
+		
+		preferenceRepository.save(preference);
+		
+		return PreferenceMapper.toDto(preference);
 	}
 
 	@Override
-	public PreferenceDto getPreferenceById(Long preferenceId) {
-		// TODO Auto-generated method stub
-		return null;
+	public PreferenceResponseDto updatePreference(Long preferenceId, PreferenceUpdateDto dto) {
+		
+		Preference preference = preferenceRepository.findById(preferenceId)
+									.orElseThrow(() -> new ResourceNotFoundException("Preference not found"));
+		
+		preference.setName(dto.getName());
+		
+		return PreferenceMapper.toDto(preference);
 	}
 
 	@Override
-	public Page<PreferenceDto> getPreferencesByUserId(Long userId, Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
+	public PreferenceResponseDto getPreferenceById(Long preferenceId) {
+		
+		Preference preference = preferenceRepository.findById(preferenceId)
+									.orElseThrow(() -> new ResourceNotFoundException("Preference not found"));
+		
+		return PreferenceMapper.toDto(preference);
 	}
 
 	@Override
-	public Page<PreferenceDto> searchPreferencesByName(String name, Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
+	public Page<PreferenceResponseDto> getPreferencesByUserId(Long userId, Pageable pageable) {
+		
+		
+		if(userId == null)
+		{
+			throw new IllegalArgumentException("User id not available");
+		}
+		
+		Page<Preference> preferences = preferenceRepository.findByUser_UserId(userId, pageable);
+		
+		if(preferences.isEmpty())
+		{
+			return Page.empty(pageable);
+		}
+		
+				
+		return preferences.map(preference -> 
+				new PreferenceResponseDto(preference.getPreferenceId(),
+						preference.getName(),
+						preference.getUser().getUserId()));
+		
+	}
+
+	@Override
+	public Page<PreferenceResponseDto> searchPreferencesByName(String name, Pageable pageable) {
+		
+		if(name == null || name.isBlank())
+		{
+			throw new IllegalArgumentException("Name not found");
+		}
+		
+		Page<Preference> preferences = preferenceRepository
+											.findByNameContainingIgnoreCase(name, pageable);
+		
+		
+		if(preferences.isEmpty())
+		{
+			return Page.empty(pageable);
+		}
+		
+		return preferences.map(preference -> 
+			new PreferenceResponseDto(preference.getPreferenceId(),preference.getName(),preference.getUser().getUserId()));
 	}
 
 	@Override
 	public void deletePreference(Long preferenceId) {
-		// TODO Auto-generated method stub
+		
+		if(preferenceId == null)
+		{
+			throw new IllegalArgumentException("Preference id not found");
+		}
+		
+		Preference preference = preferenceRepository.findById(preferenceId)
+									.orElseThrow(() -> new ResourceNotFoundException("Preference is not vailable"));
+		
+		preferenceRepository.delete(preference);
 		
 	}
+	
 
 }

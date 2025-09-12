1e6c6f95593bcb935018f6d54a945819b18c63e8
risrchanish <risrchanish@gmail.com>
2025-09-12 13:04:13 +0530

    Global exception handler

---
+++ exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package risrchanish.product.recommend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex)
+	{
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+	}
+	
+	@ExceptionHandler(DuplicateMetadataException.class)
+	public ResponseEntity<String> handleDuplicateMetadata(DuplicateMetadataException ex)
+	{
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+	}
+	
+	@ExceptionHandler(DuplicateFeatureVectorException.class)
+	public ResponseEntity<String> handleDuplicateFeatureVector(DuplicateFeatureVectorException ex)
+	{
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+	}
+	
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> handleAllExceptions(Exception ex)
+	{
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unexpected error: "+ex.getMessage());
+	}
+}

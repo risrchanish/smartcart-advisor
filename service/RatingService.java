f3b85cfa8a7942a8eec33b200b8350d474f5eb9e
risrchanish <risrchanish@gmail.com>
2025-09-12 10:05:01 +0530

    Service with pageable support for rating queries and update logic

--- service/RatingService.java
+++ service/RatingService.java
@@ -6,17 +6,20 @@ import org.springframework.data.domain.Pageable;
 import risrchanish.product.recommend.dto.rating.RatingCreateDto;
 import risrchanish.product.recommend.dto.rating.RatingResponseDto;
 import risrchanish.product.recommend.dto.rating.RatingUpdateDto;
-import risrchanish.product.recommend.entity.Rating;
 
 public interface RatingService {
 	
 	RatingResponseDto createRating(RatingCreateDto dto);
+	
+	RatingResponseDto getRatingById(Long ratingId);
 
 	Page<RatingResponseDto> getRatingsByProduct(Long productId, Pageable pageable);
 	
 	Page<RatingResponseDto> getRatingsByUser(Long userId, Pageable pageable);
 	
-	RatingResponseDto updateRatingByProductId(Long productId, RatingUpdateDto dto);
+	Page<RatingResponseDto> getAllRatings(Pageable pageable);
 	
-	void deleteRating(Long ratingId);
+	RatingResponseDto updateRating(Long productId, Long userId, RatingUpdateDto dto);
+	
+	void deleteRating(Long ratingId,Long userId, Long productId);
 }

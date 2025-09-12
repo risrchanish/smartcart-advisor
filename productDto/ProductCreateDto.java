b686ba409bca129f6343a66b89e8dbd21c227488
risrchanish <risrchanish@gmail.com>
2025-09-12 08:05:45 +0530

    Dto for product creation with metadata, ratings and feature vectors

--- productDto/ProductCreateDto.java
+++ productDto/ProductCreateDto.java
@@ -1,8 +1,12 @@
 package risrchanish.product.recommend.dto.product;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import risrchanish.product.recommend.dto.productfeature.ProductFeatureCreateDto;
 import risrchanish.product.recommend.dto.productmetadata.ProductMetadataCreateDto;
 import risrchanish.product.recommend.dto.rating.RatingCreateDto;
@@ -10,23 +14,23 @@ import risrchanish.product.recommend.dto.rating.RatingCreateDto;
 
 public class ProductCreateDto {
 
-//	@NotBlank(message = "Product name is required")
+	@NotBlank(message = "Product name is required")
 	private String name;
 	
-//	@NotNull(message = "Price must be provided")
-//	@DecimalMin(value = "0.0", inclusive = true, message = "Price must not be negative")
+	@NotNull(message = "Price must be provided")
+	@DecimalMin(value = "0.0", inclusive = true, message = "Price must not be negative")
 	private Double price;
 	
-//	@NotBlank(message = "Category is required")
+	@NotBlank(message = "Category is required")
 	private String category;
 	
 	private boolean inStock;
 	
-	private ProductMetadataCreateDto metadata;
+	private List<ProductMetadataCreateDto> metadataList = new ArrayList<>();
 	
-	private List<RatingCreateDto> ratings;
+	private List<RatingCreateDto> ratings = new ArrayList<>();
 	
-	private List<ProductFeatureCreateDto> featureVector;
+	private List<ProductFeatureCreateDto> featureVector = new ArrayList<>();
 	
 	
 	public List<RatingCreateDto> getRatings() {
@@ -45,12 +49,12 @@ public class ProductCreateDto {
 		this.featureVector = featureVector;
 	}
 
-	public ProductMetadataCreateDto getMetadata() {
-		return metadata;
+	public List<ProductMetadataCreateDto> getMetadataList() {
+		return metadataList;
 	}
 
-	public void setMetadata(ProductMetadataCreateDto metadata) {
-		this.metadata = metadata;
+	public void setMetadata(List<ProductMetadataCreateDto> metadataList) {
+		this.metadataList = metadataList;
 	}
 
 
@@ -98,7 +102,7 @@ public class ProductCreateDto {
 	@Override
 	public String toString() {
 		return "ProductCreateDto [name=" + name + ", price=" + price + ", category=" + category + ", inStock=" + inStock
-				+ ", ratings=" + ratings + ", features=" + featureVector + ", metadata=" + metadata + "]";
+				+ ", ratings=" + ratings + ", features=" + featureVector + ", metadataList=" + metadataList + "]";
 	}
 
 

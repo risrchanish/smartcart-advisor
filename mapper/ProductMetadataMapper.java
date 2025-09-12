d0b760e93b0788ad52baa4792afb913da1512eb6
risrchanish <risrchanish@gmail.com>
2025-09-12 08:48:23 +0530

    Mapper with static methods for metadata creation, update and response mapping

--- mapper/ProductMetadataMapper.java
+++ mapper/ProductMetadataMapper.java
@@ -1,7 +1,15 @@
 package risrchanish.product.recommend.mapper;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import risrchanish.product.recommend.dto.productmetadata.ProductMetadataCreateDto;
 import risrchanish.product.recommend.dto.productmetadata.ProductMetadataResponseDto;
+import risrchanish.product.recommend.dto.productmetadata.ProductMetadataUpdateDto;
 import risrchanish.product.recommend.entity.Product;
 import risrchanish.product.recommend.entity.ProductMetadata;
 
@@ -10,9 +18,29 @@ public class ProductMetadataMapper {
 //	MetadataCreateDto  ----> ProductMetadata
 	public static ProductMetadata toProductMetadata(ProductMetadataCreateDto dto, Product product)
 	{
+		
+		if(dto == null)
+		{
+			return null;
+		}
+		
 		ProductMetadata metadata = new ProductMetadata();
 		
-		metadata.setAdditionalAttributes(dto.getAdditionalAttributes());
+		
+		metadata.setAdditionalAttributes(
+			    Optional.ofNullable(dto.getAdditionalAttributes())
+			            .map(map -> new HashMap<>(map))
+			            .orElse(new HashMap<>())
+			);
+
+			metadata.setTags(
+			    Optional.ofNullable(dto.getTags())
+			            .map(list -> new ArrayList<>(list))
+			            .orElse(new ArrayList<>())
+			);	
+			
+			
+			
 		metadata.setBrand(dto.getBrand());
 		metadata.setColor(dto.getColor());
 		metadata.setMaterial(dto.getMaterial());
@@ -20,9 +48,21 @@ public class ProductMetadataMapper {
 		metadata.setMinPrice(dto.getMinPrice());
 		metadata.setProduct(product);
 		metadata.setSize(dto.getSize());
-		metadata.setTags(dto.getTags());
+		
 		
 		return metadata;
+	}
+	
+	
+	// List type mapper overloaded
+	
+	public static List<ProductMetadata> toProductMetadata(List<ProductMetadataCreateDto> dtoList, Product product) 
+	{
+	    return Optional.ofNullable(dtoList)
+	        .orElse(Collections.emptyList())
+	        .stream()
+	        .map(dto -> toProductMetadata(dto, product)) 
+	        .collect(Collectors.toList());
 	}
 	
 //	ProductMetadata ----------> ProductMetadataResponseDto
@@ -36,9 +76,29 @@ public class ProductMetadataMapper {
 												metadata.getColor(),
 												metadata.getSize(),
 												metadata.getMaterial(),
-												metadata.getTags(),
-												metadata.getAdditionalAttributes()
-											);
+												metadata.getMaxPrice(),
+												metadata.getMinPrice(),
+												Optional.ofNullable(metadata.getTags())
+												.map(list -> new ArrayList<>(list)).orElse(new ArrayList<>()),
+												Optional.ofNullable(metadata.getAdditionalAttributes())
+												.map(map -> new HashMap<>(map)).orElse(new HashMap<>()));
+											
 		return dto;
+	}
+	
+	// UpdateDto  -----> toProductMetadata
+	public static ProductMetadata toUpdateProductMetadata(ProductMetadata metadata, ProductMetadataUpdateDto dto, Product product)
+	{
+		metadata.setBrand(dto.getBrand());
+		metadata.setColor(dto.getColor());
+		metadata.setMaterial(dto.getMaterial());
+		metadata.setMaxPrice(dto.getMaxPrice());
+		metadata.setMinPrice(dto.getMinPrice());
+		metadata.setSize(dto.getSize());
+		metadata.setProduct(product);
+		metadata.setAdditionalAttributes(Optional.ofNullable(dto.getAdditionalAttributes()).orElse(Collections.emptyMap()));
+		metadata.setTags(Optional.ofNullable(dto.getTags()).orElse(Collections.emptyList()));
+		
+		return metadata;
 	}
 }

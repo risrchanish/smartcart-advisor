28409b7206c507a9f524e50e333d7b69cce1d030
risrchanish <risrchanish@gmail.com>
2025-09-12 08:29:37 +0530

    Exception for failure handling

--- exception/ResourceNotFoundException.java
+++ exception/ResourceNotFoundException.java
@@ -2,7 +2,7 @@ package risrchanish.product.recommend.exception;
 
 public class ResourceNotFoundException extends RuntimeException{
 	
-	public ResourceNotFoundException(String message)
+	public ResourceNotFoundException(String message )
 	{
 		super(message);
 	}

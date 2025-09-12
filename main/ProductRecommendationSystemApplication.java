4bc21de69822211ae931ca6451965ee7754ca65c
risrchanish <risrchanish@gmail.com>
2025-09-12 11:26:42 +0530

    Main method

---
+++ main/ProductRecommendationSystemApplication.java
@@ -0,0 +1,16 @@
+package risrchanish.product.recommend;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRecommendationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRecommendationSystemApplication.class, args);
+		
+		System.out.println("App is running..");
+	}
+
+}

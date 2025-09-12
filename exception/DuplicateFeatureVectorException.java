aa632bc26d99ae41b59df1806d21158d0906ff3e
risrchanish <risrchanish@gmail.com>
2025-09-12 08:31:54 +0530

    Exception handeling in feature vector ingestion

---
+++ exception/DuplicateFeatureVectorException.java
@@ -0,0 +1,9 @@
+package risrchanish.product.recommend.exception;
+
+public class DuplicateFeatureVectorException extends RuntimeException{
+
+	public DuplicateFeatureVectorException(String message)
+	{
+		super(message);
+	}
+}

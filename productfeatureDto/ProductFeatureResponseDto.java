dd4f25bf8eefae30d1d36e76b01d2f1567822259
risrchanish <risrchanish@gmail.com>
2025-09-12 07:16:56 +0530

    Record type for serialization of product feature details

--- productfeatureDto/ProductFeatureResponseDto.java
+++ productfeatureDto/ProductFeatureResponseDto.java
@@ -2,7 +2,6 @@ package risrchanish.product.recommend.dto.productfeature;
 
 import java.util.List;
 
-import risrchanish.product.recommend.entity.Product;
 
 public record ProductFeatureResponseDto(
 		Long featureId,
